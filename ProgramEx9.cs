95275e00b543f48970a7d0ae6979aaa6d1acc767
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 19:40:29 -0300

    Add files via upload

---
+++ ProgramEx9.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1POOex8
+{
+    internal class retangulo
+    {
+
+        private double h;
+        private double b;
+        private double a;
+
+        public void seth(double ba)
+        {
+            h = ba;
+
+        }
+        public void setb(double ba)
+        {
+            b = ba;
+
+        }
+        public double geth()
+        {
+            return h;
+        }
+        public double getb()
+        {
+            return b;
+        }
+
+        public double geta()
+        {
+            return a;
+        }
+
+        public void calcular()
+        {
+            a = b * h / 2;
+            if (a < 100)
+                Console.WriteLine("Terreno pequeno", a);
+            else
+                if (a > 100)
+                Console.WriteLine("terreno grande", a);
+        }
+    }
+}
